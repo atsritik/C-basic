6eb97b31c09db8089429f52e6aaeb0cded4c2db7
Ritik vats <103229408+atsritik@users.noreply.github.com>
2024-10-02 01:10:01 +0530

    Add files via upload

---
+++ posneg.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+    int x;
+    printf("enter any number");
+    scanf("%d",&x);
+    if(x>=0 )
+    {
+        printf("no. is positive");
+    }
+else {
+    printf("no. is non  positive");
+}
+}
