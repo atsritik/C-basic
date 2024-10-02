8af34ba3c7ffa2e747523cdf123bfc7a5fb5b2b8
Ritik vats <103229408+atsritik@users.noreply.github.com>
2024-10-02 13:52:08 +0530

    Add files via upload

---
+++ si.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+    int p,t,si;
+    float rate;
+    printf("enter a princle ,time and rate ");
+    scanf("%d%d%f",&p,&t,&rate);
+   si=(p*rate*t)/100;
+   printf("%d",si);
+    
+}
+
+     
