324c11ca519b42a48a39979367b3d61edc755325
Ritik vats <103229408+atsritik@users.noreply.github.com>
2024-10-08 22:10:10 +0530

    Add files via upload

---
+++ oddnumbersum.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+
+    int sum=0,num,i,m,n;
+    printf("Enter any number");
+    scanf("%d",&num);
+    for(i=1;i<=num;i++)
+    {
+        printf("%d",m=2*i-1);
+        sum=sum+m;
+
+
+    }
+
+printf("sum %d",sum);
+}
